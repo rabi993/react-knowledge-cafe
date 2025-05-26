81f9ba13055ef207fc24e26a1fbfb93ffd64ff34
rabi993 <rabi.21.pust@gmail.com>
2025-05-26 12:23:58 +0600

    42-5 Add Bookmark button and lift up the state

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
-
-const Blog = ({blog}) => {
+import {FaBookmark } from 'react-icons/fa';
+const Blog = ({blog, handleAddToBookmark}) => {
     const {title,cover,author_img,reading_time,author,posted_date,hashtags} = blog;
     console.log(blog);
 
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of title : ${title}`} />
+        <div className='mb-20'>
+            <img className='w-full mb-4' src={cover} alt={`Cover picture of title : ${title}`} />
 
-            <div className='flex justify-between'>
-                <div className='flex'>
+            <div className='flex justify-between  mb-8'>
+                <div className='flex '>
                     <img className='w-14' src={author_img} alt="author_img" />
                     <div className='ml-6'>
                         <h3 className='text-2xl'>{author}</h3>
@@ -18,9 +18,10 @@ const Blog = ({blog}) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button onClick={handleAddToBookmark} className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
                 </div>
             </div>
-            <h1 className='text-4xl'>{title}</h1>
+            <h1 className='text-4xl mb-4'>{title}</h1>
             <p>
                 {
                     hashtags.map((hash,idx) => <span key={idx}> <a href="">{hash}</a></span>)
